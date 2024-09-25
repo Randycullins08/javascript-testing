bdeed46ada96cffc6f4ca439508e347204449f90
Randy Cullins <randycullins2018@gmail.com>
2024-09-25 06:31:41 -0600

    Practice with test driven development by building tests for calculateAverage function first

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,6 +1,6 @@
 import { describe, test, it, expect } from "vitest";
 
-import { fizzBuzz, max } from "../src/intro";
+import { calculateAverage, fizzBuzz, max } from "../src/intro";
 
 // Example of testing pattern:
 // describe("max", () => {
@@ -47,5 +47,23 @@ describe("fizzbuzz", () => {
 
   it("should return a string version of the argument if it's not divisible by either three or five", () => {
     expect(fizzBuzz(1)).toBe("1");
+  });
+});
+
+describe("calculateAverage", () => {
+  it("should return NaN if given an empty array", () => {
+    expect(calculateAverage([])).toBe(NaN);
+  });
+
+  it("should calculate the average of an array with a single element", () => {
+    expect(calculateAverage([1])).toBe(1);
+  });
+
+  it("should calculate the average of an array with two elements", () => {
+    expect(calculateAverage([1, 2])).toBe(1.5);
+  });
+
+  it("should calculate the average of an array with three elements", () => {
+    expect(calculateAverage([1, 2, 3])).toBe(2);
   });
 });
